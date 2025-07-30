14916bed5cfabef749be35be17e4dd753f5d1347
HITHA HARISH <hitha22harish@gmail.com>
2025-07-31 00:04:32 +0530

    Add files via upload

---
+++ tailwand.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          lightgray: '#d3d3d3',
+        },
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
