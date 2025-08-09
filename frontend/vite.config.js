51cb937f7bc28e62c68e67f67964d0c4b6deaef7
HITHA HARISH <hitha22harish@gmail.com>
2025-08-09 11:56:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Your backend in development
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
+  build: {
+    outDir: '../frontend/dist', // Explicitly set build directory
+    emptyOutDir: true, // Clears old files on rebuild
+  }
 })
