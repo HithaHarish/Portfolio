d0000db12c4853a3603e88ec4e1c7a49199e0e43
HITHA HARISH <hitha22harish@gmail.com>
2025-08-11 16:38:25 +0530

    Update contact.js

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,17 +1,24 @@
-import nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer'; // Library to send email 
+//(an SMTP - Simple Mail Transfer Protocol client for Node JS) 
+// It sends mails between servers or an application to the server (here)
 
-export const handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
+export const handler = async (event) => { //async indicates the function can use await - for asynchronous functions
+  //event is passed when the function is triggered in frontend or by netlify
+  if (event.httpMethod !== 'POST') { //check if event passed has method POST only
+
+    // Wr use res.send() in Express, in serverless we send with return 
     return {
-      statusCode: 405,
+      statusCode: 405, // request method is not supported
       body: JSON.stringify({ error: 'Method not allowed' }),
+      // convert { error: 'Method not allowed' } (JSON object) to a string and display as body
     };
   }
 
   try {
-    const { name, email, message } = JSON.parse(event.body);
+    const { name, email, message } = JSON.parse(event.body); //parse destructures the event's body into NEM constants
 
     const transporter = nodemailer.createTransport({
+      //transporter is a SMTP Client configured to send emails
       service: 'gmail', // or your SMTP host
       auth: {
         user: 'hitha22harish@gmail.com',
@@ -20,17 +27,22 @@ export const handler = async (event) => {
     });
 
     await transporter.sendMail({
+      // await pauses the javascript here so it doesnt move to return before the reponse is received
       from: email,
       to: 'hitha22harish@gmail.com',
       subject: `Hitha, ${name} tried to connect with you. Check it , could be important !`,
       text: message,
     });
 
-    return {
+    // So, this await function will return a Promise that will either be resolved or rejected
+    // this Promise is resolved or rejected and will throw an error in try if rejected else it will continue to return if resolved
+
+    return { // if no error is raised till here in try returns success
       statusCode: 200,
       body: JSON.stringify({ success: true, message: 'I received your message, I will get back to you !' }),
     };
-  } catch (error) {
+  } catch (error) { //any error in try is caught here 
+    // if the message isnt sent and an error raises, it will enter catch and print the following 
     console.error('Email sending error:', error);
     return {
       statusCode: 500,
