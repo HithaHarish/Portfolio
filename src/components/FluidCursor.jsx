7acd1a1b408e4d0df9f6e041706a4b98bfab8688
HITHA HARISH <hitha22harish@gmail.com>
2025-08-11 20:28:29 +0530

    Update FluidCursor.jsx

--- src/components/FluidCursor.jsx
+++ src/components/FluidCursor.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import fluidCursor from '../hooks/use-FluidCursor';
+import { useEffect } from 'react'; //A React hook that lets you run side effects after the component renders
+import fluidCursor from '../hooks/use-FluidCursor'; //animation, event listener and drawings
 import '../styles/FluidCursor.css'; // <- Import the styling
 
 const FluidCursor = () => {
